6818779160e8c2bd22c2be544ceac09411e14c5a
XianReallyHot-ZZH <741788769@qq.com>
2025-02-08 20:18:13 +0800

    init:初始化example模块

---
+++ hawk4j-example/src/main/java/com/yy/hawk4j/example/Hawk4jExampleApplication.java
@@ -0,0 +1,13 @@
+package com.yy.hawk4j.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hawk4jExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hawk4jExampleApplication.class, args);
+	}
+
+}
---
+++ hawk4j-example/src/test/java/com/yy/hawk4j/example/Hawk4jExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yy.hawk4j.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hawk4jExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
